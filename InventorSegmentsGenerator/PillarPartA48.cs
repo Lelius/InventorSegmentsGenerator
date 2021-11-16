00c2624c62b228345d46d59d021808ff72b8c342
Lelius <leliuslogunov@gmail.com>
2021-11-16 10:04:43 +0200

    Готовый класс PillarPartA48 с учётом угла, а также отвестия для крепежа и вкл/выкл отвестий для крепления в грунт. Под вопросом разделения формирования детали отдельными методами. Но на данный момент класс уже рабочий.

--- InventorSegmentsGenerator/PillarPartA48.cs
+++ InventorSegmentsGenerator/PillarPartA48.cs
@@ -7,25 +7,34 @@ namespace InventorSegmentsGenerator
 {
     public class PillarPartA48 : ProfileA48
     {
+        const double HeightProfile = 3.2;
         public enum TypeOfFastening { Boot, Ground, Rack }
 
+        private double basicAngle;
+        public double BasicAngle
+        {
+            get { return basicAngle; }
+            set { basicAngle = value; }
+        }
+
         public double heightPillar;
-        public double basicAngle;
         public int numberOfHoles;
         public Dictionary<int, double> distanceToHoles;
         public TypeOfFastening typeOfFastening;
-        public double holeDiametr;
+        public double holeBoltDiametr;
+        public double holeGroundDiametr;
 
         protected Face oFrontFace;
 
         public PillarPartA48(Inventor.Application invApp) : base(invApp, 120, "Стеклопластик", "Оранжевый")
         {
             heightPillar = 120;
-            basicAngle = 0;
             numberOfHoles = 3;
             distanceToHoles = new Dictionary<int, double> { { 1, 3.6}, { 2, 45}, { 3, 95} };
             typeOfFastening = TypeOfFastening.Boot;
-            holeDiametr = 0.9;
+            holeBoltDiametr = 0.9;
+            holeGroundDiametr = 0.52;
+            basicAngle = 0;
         }
 
         public PillarPartA48(Inventor.Application invApp, double heightPillar, string materialProfile, string colorProfile, TypeOfFastening typeOfFastening) :
@@ -33,48 +42,104 @@ namespace InventorSegmentsGenerator
 
         {
             this.heightPillar = heightPillar;
-            basicAngle = 0;
             numberOfHoles = 3;
             distanceToHoles = new Dictionary<int, double> { { 1, 3.6 }, { 2, 45 }, { 3, 95 } };
             this.typeOfFastening = typeOfFastening;
-            holeDiametr = 0.9;
+            holeBoltDiametr = 0.9;
+            holeGroundDiametr = 0.52;
+            basicAngle = 0;
         }
 
         public void createPillarPart()
         {
+            basicAngle = 25;
+            typeOfFastening = TypeOfFastening.Ground;
             createProfile();
-            createBoltHoles();
-            basicAngle = -30;
             createAngleCut();
+            createBoltHoles();
+            createGroundHoles();
         }
 
 
+        private void createGroundHoles()
+        {
+            switch (typeOfFastening)
+            {
+                case TypeOfFastening.Boot:
+                    break;
+                case TypeOfFastening.Ground:
+                    {
+                        ExtrudeFeature extrudeFeature = oCompDef.Features.ExtrudeFeatures["Выдавливание1"];
+                        foreach (Face oF in extrudeFeature.SideFaces)
+                        {
+                            Point wPoint = oF.PointOnFace;
+                            if (wPoint.Y == point[7].Y & oF.SurfaceType == SurfaceTypeEnum.kPlaneSurface)
+                            {
+                                foreach (Edge oEdge in oF.Edges)
+                                {
+                                    if (oEdge.PointOnEdge.X == 0 & oEdge.PointOnEdge.Y == 0)
+                                    {
+                                        oSketch = oCompDef.Sketches.AddWithOrientation(oF, oCompDef.WorkAxes[3], true, false, oEdge.StopVertex);
+                                        Point2d pointCenter = oTransGeo.CreatePoint2d((oLine[7].Length / 2), 10);
+                                        SketchCircle circle = oSketch.SketchCircles.AddByCenterRadius(pointCenter, holeGroundDiametr / 2);
+
+                                        oProfile = oSketch.Profiles.AddForSolid();
+                                        oExtrudeDef = oCompDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(oProfile, PartFeatureOperationEnum.kCutOperation);
+                                        oExtrudeDef.SetDistanceExtent(oLine[0].Length, PartFeatureExtentDirectionEnum.kNegativeExtentDirection);
+                                        oExtrude = oCompDef.Features.ExtrudeFeatures.Add(oExtrudeDef);
+                                    }
+                                }
+                            }
+                            if (wPoint.X == point[1].X & oF.SurfaceType == SurfaceTypeEnum.kPlaneSurface)
+                            {
+                                foreach (Edge oEdge in oF.Edges)
+                                {
+                                    if (oEdge.PointOnEdge.X == 0 & oEdge.PointOnEdge.Y == 0)
+                                    {
+                                        oSketch = oCompDef.Sketches.AddWithOrientation(oF, oCompDef.WorkAxes[3], false, false, oEdge.StopVertex);
+                                        Point2d pointCenter = oTransGeo.CreatePoint2d((oLine[0].Length / 2), -5);
+                                        SketchCircle circle = oSketch.SketchCircles.AddByCenterRadius(pointCenter, holeGroundDiametr / 2);
+
+                                        oProfile = oSketch.Profiles.AddForSolid();
+                                        oExtrudeDef = oCompDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(oProfile, PartFeatureOperationEnum.kCutOperation);
+                                        oExtrudeDef.SetDistanceExtent(oLine[7].Length, PartFeatureExtentDirectionEnum.kNegativeExtentDirection);
+                                        oExtrude = oCompDef.Features.ExtrudeFeatures.Add(oExtrudeDef);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case TypeOfFastening.Rack:
+                    break;
+                default:
+                    return;
+            }
+        }
+
         private void createAngleCut()
         {
-            double cathe = Math.Abs(oLine[0].Length * Math.Tan(basicAngle * Math.PI / 180)); //угол в радианах
-
-            if (Math.Abs(basicAngle) < 0.0000001)
+            if (Math.Abs(BasicAngle) < 0.0000001)
             {
                 return;
             }
-            else if (basicAngle > 0)
+            else if (BasicAngle > 0)
             {
                 ExtrudeFeature extrudeFeature = oCompDef.Features.ExtrudeFeatures["Выдавливание1"];
-                extrudeFeature.Definition.SetDistanceExtent(heightPillar + cathe, PartFeatureExtentDirectionEnum.kNegativeExtentDirection);
+                extrudeFeature.Definition.SetDistanceExtent(heightPillar + getCathe(BasicAngle), PartFeatureExtentDirectionEnum.kNegativeExtentDirection);
 
                 foreach (Face oF in extrudeFeature.SideFaces)
                 {
                     Point wPoint = oF.PointOnFace;
                     if (wPoint.X == point[6].X)
                     {
-                        Point sketchPoint = oTransGeo.CreatePoint(point[6].X, 0, 0);
                         oSketch = oCompDef.Sketches.AddWithOrientation(oF, oF.Edges[1], true, true, oF.Edges[1].StartVertex);
 
                         Point2d[] wP = new Point2d[3];
                         SketchLine[] wL = new SketchLine[3];
 
                         wP[0] = oTransGeo.CreatePoint2d(0, 0);
-                        wP[1] = oTransGeo.CreatePoint2d(3.2, cathe);
+                        wP[1] = oTransGeo.CreatePoint2d(3.2, getCathe(BasicAngle));
                         wP[2] = oTransGeo.CreatePoint2d(3.2, 0);
 
                         wL[0] = oSketch.SketchLines.AddByTwoPoints(wP[0], wP[1]);
@@ -88,7 +153,7 @@ namespace InventorSegmentsGenerator
                     }
                 }
             }
-            else if (basicAngle < 0)
+            else if (BasicAngle < 0)
             {
                 ExtrudeFeature extrudeFeature = oCompDef.Features.ExtrudeFeatures["Выдавливание1"];
 
@@ -104,7 +169,7 @@ namespace InventorSegmentsGenerator
                         SketchLine[] wL = new SketchLine[3];
 
                         wP[0] = oTransGeo.CreatePoint2d(0, 0);
-                        wP[1] = oTransGeo.CreatePoint2d(0, cathe);
+                        wP[1] = oTransGeo.CreatePoint2d(0, getCathe(BasicAngle));
                         wP[2] = oTransGeo.CreatePoint2d(3.2, 0);
 
                         wL[0] = oSketch.SketchLines.AddByTwoPoints(wP[0], wP[1]);
@@ -123,22 +188,24 @@ namespace InventorSegmentsGenerator
 
         private void createBoltHoles()
         {
-            foreach (Face oF in oExtrude.SideFaces)
-            {
-                Box2d box = oF.Evaluator.ParamRangeRect;
+            ExtrudeFeature extrudeFeature = oCompDef.Features.ExtrudeFeatures["Выдавливание1"];
 
-                if (box.MinPoint.X == point[0].X)
-                    if (box.MinPoint.Y == point[0].Y)
-                        if (box.MaxPoint.X == 4.8)
-                            if (box.MaxPoint.Y == heightPillar)
-                                oFrontFace = oF;
+            foreach (Face oF in extrudeFeature.SideFaces)
+            {
+                Point wPoint = oF.PointOnFace;
+                if(wPoint.Y == 0.0)
+                {
+                    oFrontFace = oF;
+                }
             }
+
+            WorkPoint workPoint = oCompDef.WorkPoints[1];
 
             for (int i = 1; i <= distanceToHoles.Count; i++)
             {
                 oSketch = oCompDef.Sketches.AddWithOrientation(oFrontFace, oCompDef.WorkAxes[1], true, true, oCompDef.WorkPoints[1]);
-                Point2d pointCenter = oTransGeo.CreatePoint2d(2.4, 0 - distanceToHoles[i]);
-                SketchCircle circle = oSketch.SketchCircles.AddByCenterRadius(pointCenter, holeDiametr / 2);
+                Point2d pointCenter = oTransGeo.CreatePoint2d(2.4, 0 - distanceToHoles[i] - getCathe(BasicAngle));
+                SketchCircle circle = oSketch.SketchCircles.AddByCenterRadius(pointCenter, holeBoltDiametr / 2);
                 oProfile = oSketch.Profiles.AddForSolid();
                 foreach (ProfilePath oProfPath in oProfile)
                 {
@@ -153,6 +220,14 @@ namespace InventorSegmentsGenerator
                 oExtrudeDef.SetDistanceExtent(0.5, PartFeatureExtentDirectionEnum.kNegativeExtentDirection);
                 oExtrude = oCompDef.Features.ExtrudeFeatures.Add(oExtrudeDef);
             }
+        }
+
+
+        static private protected double getCathe(double angle)
+        {
+            if (angle == 0)
+                return 0;
+            return (double)Math.Abs(HeightProfile * Math.Tan(angle * Math.PI / 180));
         }
     }
 }

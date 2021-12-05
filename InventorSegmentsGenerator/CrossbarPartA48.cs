36bbc961427cfc11ce68641c6d8f9dc0bdcc8c7b
Lelius <leliuslogunov@gmail.com>
2021-12-05 22:02:05 +0200

    Crossbar. Сделал второй шип перекладины.

--- InventorSegmentsGenerator/CrossbarPartA48.cs
+++ InventorSegmentsGenerator/CrossbarPartA48.cs
@@ -18,7 +18,11 @@ namespace InventorSegmentsGenerator
 
         protected Point2d[] oPointsNear;
         protected SketchLine[] oLinesNear;
-        protected PlanarSketch oSketchSpikes;
+        protected PlanarSketch oSketchSpikesNear;
+
+        protected Point2d[] oPointsFar;
+        protected SketchLine[] oLinesFar;
+        protected PlanarSketch oSketchSpikesFar;
 
         public CrossbarPartA48(Inventor.Application InvApp, double lengthCrossbar) : 
             base (InvApp, lengthCrossbar + 10, "Стеклопластик", "Оранжевый")                    // + 100 мм на случай наклонной перекладины
@@ -47,7 +51,7 @@ namespace InventorSegmentsGenerator
             WorkAxis oWorkAxisCrossbar = oCompDef.WorkAxes.AddByTwoPoints(oWorkPointOne, oWorkPointTwo, true);
             WorkPlane oWorkPlaneCrossbar = oCompDef.WorkPlanes.AddByLinePlaneAndAngle(oWorkAxisCrossbar, oCompDef.WorkPlanes["Плоскость XZ"], BasicAngle, true);
 
-            oSketchSpikes = oCompDef.Sketches.AddWithOrientation(oWorkPlaneCrossbar, oWorkAxisCrossbar, true, true, oWorkPointOne);
+            oSketchSpikesNear = oCompDef.Sketches.AddWithOrientation(oWorkPlaneCrossbar, oWorkAxisCrossbar, true, true, oWorkPointOne);
 
             oPointsNear = new Point2d[10];
             oPointsNear[0] = oTransGeo.CreatePoint2d(0, -10);
@@ -62,19 +66,52 @@ namespace InventorSegmentsGenerator
             oPointsNear[9] = oTransGeo.CreatePoint2d(0, 2.7);
 
             oLinesNear = new SketchLine[10];
-            oLinesNear[0] = oSketchSpikes.SketchLines.AddByTwoPoints(oPointsNear[0], oPointsNear[1]);
-            oLinesNear[1] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[0].EndSketchPoint, oPointsNear[2]);
-            oLinesNear[2] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[1].EndSketchPoint, oPointsNear[3]);
-            oLinesNear[2] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[1].EndSketchPoint, oPointsNear[3]);
-            oLinesNear[3] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[2].EndSketchPoint, oPointsNear[4]);
-            oLinesNear[4] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[3].EndSketchPoint, oPointsNear[5]);
-            oLinesNear[5] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[4].EndSketchPoint, oPointsNear[6]);
-            oLinesNear[6] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[5].EndSketchPoint, oPointsNear[7]);
-            oLinesNear[7] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[6].EndSketchPoint, oPointsNear[8]);
-            oLinesNear[8] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[7].EndSketchPoint, oPointsNear[9]);
-            oLinesNear[9] = oSketchSpikes.SketchLines.AddByTwoPoints(oLinesNear[8].EndSketchPoint, oLinesNear[0].StartSketchPoint);
+            oLinesNear[0] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oPointsNear[0], oPointsNear[1]);
+            oLinesNear[1] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[0].EndSketchPoint, oPointsNear[2]);
+            oLinesNear[2] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[1].EndSketchPoint, oPointsNear[3]);
+            oLinesNear[2] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[1].EndSketchPoint, oPointsNear[3]);
+            oLinesNear[3] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[2].EndSketchPoint, oPointsNear[4]);
+            oLinesNear[4] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[3].EndSketchPoint, oPointsNear[5]);
+            oLinesNear[5] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[4].EndSketchPoint, oPointsNear[6]);
+            oLinesNear[6] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[5].EndSketchPoint, oPointsNear[7]);
+            oLinesNear[7] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[6].EndSketchPoint, oPointsNear[8]);
+            oLinesNear[8] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[7].EndSketchPoint, oPointsNear[9]);
+            oLinesNear[9] = oSketchSpikesNear.SketchLines.AddByTwoPoints(oLinesNear[8].EndSketchPoint, oLinesNear[0].StartSketchPoint);
 
-            oProfile = oSketchSpikes.Profiles.AddForSolid();
+            oProfile = oSketchSpikesNear.Profiles.AddForSolid();
+            oExtrudeDef = oCompDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(oProfile, PartFeatureOperationEnum.kCutOperation);
+            oExtrudeDef.SetThroughAllExtent(PartFeatureExtentDirectionEnum.kSymmetricExtentDirection);
+            oExtrude = oCompDef.Features.ExtrudeFeatures.Add(oExtrudeDef);
+
+
+            oSketchSpikesFar = oCompDef.Sketches.AddWithOrientation(oWorkPlaneCrossbar, oWorkAxisCrossbar, true, true, oWorkPointOne);
+
+            oPointsFar = new Point2d[10];
+            oPointsFar[0] = oTransGeo.CreatePoint2d(0, lengthCrossbar + 10);
+            oPointsFar[1] = oTransGeo.CreatePoint2d(4.8, lengthCrossbar + 10);
+            oPointsFar[2] = oTransGeo.CreatePoint2d(4.8, lengthCrossbar - 2.7);
+            oPointsFar[3] = oTransGeo.CreatePoint2d(4.3, lengthCrossbar - 2.7);
+            oPointsFar[4] = oTransGeo.CreatePoint2d(4.3, lengthCrossbar - 0.5);
+            oPointsFar[5] = oTransGeo.CreatePoint2d(3.8, lengthCrossbar);
+            oPointsFar[6] = oTransGeo.CreatePoint2d(1, lengthCrossbar);
+            oPointsFar[7] = oTransGeo.CreatePoint2d(0.5, lengthCrossbar - 0.5);
+            oPointsFar[8] = oTransGeo.CreatePoint2d(0.5, lengthCrossbar - 2.7);
+            oPointsFar[9] = oTransGeo.CreatePoint2d(0, lengthCrossbar - 2.7);
+
+            oLinesFar = new SketchLine[10];
+            oLinesFar[0] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oPointsFar[0], oPointsFar[1]);
+            oLinesFar[1] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[0].EndSketchPoint, oPointsFar[2]);
+            oLinesFar[2] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[1].EndSketchPoint, oPointsFar[3]);
+            oLinesFar[2] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[1].EndSketchPoint, oPointsFar[3]);
+            oLinesFar[3] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[2].EndSketchPoint, oPointsFar[4]);
+            oLinesFar[4] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[3].EndSketchPoint, oPointsFar[5]);
+            oLinesFar[5] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[4].EndSketchPoint, oPointsFar[6]);
+            oLinesFar[6] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[5].EndSketchPoint, oPointsFar[7]);
+            oLinesFar[7] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[6].EndSketchPoint, oPointsFar[8]);
+            oLinesFar[8] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[7].EndSketchPoint, oPointsFar[9]);
+            oLinesFar[9] = oSketchSpikesFar.SketchLines.AddByTwoPoints(oLinesFar[8].EndSketchPoint, oLinesFar[0].StartSketchPoint);
+
+            oProfile = oSketchSpikesFar.Profiles.AddForSolid();
             oExtrudeDef = oCompDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(oProfile, PartFeatureOperationEnum.kCutOperation);
             oExtrudeDef.SetThroughAllExtent(PartFeatureExtentDirectionEnum.kSymmetricExtentDirection);
             oExtrude = oCompDef.Features.ExtrudeFeatures.Add(oExtrudeDef);

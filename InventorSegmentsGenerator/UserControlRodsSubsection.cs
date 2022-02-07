f8d9d719e72fe92a66b0d1a097d1d9d627f82070
Lelius <leliuslogunov@gmail.com>
2022-02-07 22:08:04 +0200

    Каркас класса UserConrolRodsSubsection.

---
+++ InventorSegmentsGenerator/UserControlRodsSubsection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Inventor;
+using System.Windows.Forms;
+
+namespace InventorSegmentsGenerator
+{
+    /// <summary>
+    /// Класс, содержащий таблицу элементов управления параметрами прутков-труб, упакованную в UserControl.
+    /// </summary>
+    public class UserControlRodsSubsection : UserControl
+    {
+        /// <summary>
+        /// Максимальное количество подсекций.
+        /// </summary>
+        static public int MaxNumberSubsection = 10;
+        /// <summary>
+        /// Текущий номер подсекции.
+        /// </summary>
+        private int currentNumberSubsection;
+        public int CurrentNumberSubsection
+        {
+            get => currentNumberSubsection;
+            set
+            {
+                if (value <= 0 || value > MaxNumberSubsection)
+                    throw new ArgumentException("Некорректное значение номера подсекции (UserControlRodsSubsection).");
+                currentNumberSubsection = value;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Класс, содержащий таблицу элементов управления параметрами прутков-труб, упакованную в UserControl.
+        /// </summary>
+        public UserControlRodsSubsection()
+        {
+
+        }
+    }
+}

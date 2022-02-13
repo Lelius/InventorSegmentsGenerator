6b1cad471fbb192d34c27607ae5064b4b7501038
Lelius <leliuslogunov@gmail.com>
2022-02-13 22:24:38 +0200

    Определил часть свойств в классе UserControlRodsSubsection. Остались два свойства (см. TODO:).

--- InventorSegmentsGenerator/UserControlRodsSubsection.cs
+++ InventorSegmentsGenerator/UserControlRodsSubsection.cs
@@ -27,8 +27,66 @@ namespace InventorSegmentsGenerator
                 currentNumberSubsection = value;
             }
         }
+        /// <summary>
+        /// Диаметр прутков-труб вертикального перекрытия секции.
+        /// </summary>
+        private double  diameterRods;
+        public double DiameterRods
+        {
+            get { return diameterRods; }
+            set 
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Некорректное значение величины диаметра прутка-трубы.");
+                diameterRods = value;
+            }
+        }
+        /// <summary>
+        /// Толщина стенки прутков-труб вертикального перекрытия секции.
+        /// </summary>
+        private double wallThicknessRods;
+        public double WallThicknessRods
+        {
+            get { return wallThicknessRods; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Некорректное значение величины толщины стенки трубы.");
+                wallThicknessRods = value;
+            }
+        }
+        /// <summary>
+        /// Количество коротких прутков-труб вертикального перекрытия секции.
+        /// </summary>
+        private int numberShortRods;
+        public int NumberShortRods
+        {
+            get { return numberShortRods; }
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentException("Некорректное значение количества коротких прутков-труб.");
+                numberShortRods = value; 
+            }
+        }
+        /// <summary>
+        /// Количество длинных прутков-труб вертикального перекрытия секции.
+        /// </summary>
+        private int numberLongRods;
+        public int NumberLongRods
+        {
+            get { return numberLongRods; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Некорректное значение количества длинных прутков-труб.");
+                numberLongRods = value;
+            }
+        }
+        //TODO: определить два свойства: формула заполнения и расстояния до прутков-труб
 
-        public 
+        public TypeRodsSectionEnum TypeRodsSection;
+        public MethodOfFillingRodsEnum MethodOfFillingRodsEnum;
 
         /// <summary>
         /// Класс, содержащий таблицу элементов управления параметрами прутков-труб, упакованную в UserControl.
